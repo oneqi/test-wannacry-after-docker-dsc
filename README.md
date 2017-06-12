ee2a4af7d3cfc8f0275cc6c87947628f86ce374c
oneqi <jassbansal@gmail.com>
2017-06-12 21:41:37 +0100

    additions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# test-wannacry-after-docker-dsc" 
+# test-wannacry-after-docker-dsc
+
+Sample DSC config to add to relevant nodes  
+After creating the DSC pull sever from the test-docker-dsc project, this could be pushed to relevant nodes about 15 minutes  
---
+++ Security.ps1
@@ -0,0 +1,21 @@
+﻿Configuration Security
+
+{
+
+    Registry DisableSMB1
+
+    {
+
+        Ensure      = "Present" 
+
+        Key         = "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters"
+
+        ValueName   = "SMB1"
+
+        ValueData   = "0"
+
+        ValueType   = "Dword"
+
+    }
+
+}
